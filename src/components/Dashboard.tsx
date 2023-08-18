d60092089684685f2c0198612ea7429198726361
Daniela Muniz <danielamuniz367@gmail.com>
2023-08-18 19:25:56 -0400

    joined node.js app into nextjs app

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,14 +1,15 @@
+import React from "react";
 import DashboardKPI from "./DashboardKPI";
 import DashboardTable from "./DashboardTable";
-import { Device, DashboardData } from "@/types/common";
+import { DashboardData } from "@/types/common";
 
-export default function Dashboard(props: DashboardData) {
-  const { average, tableData } = props;
-
+const Dashboard: React.FC<DashboardData> = ({ average, tableData }) => {
   return (
     <div className="flex flex-col-reverse sm:flex-row gap-4">
       <DashboardTable tableData={tableData} />
       <DashboardKPI average={average} />
     </div>
   );
-}
+};
+
+export default Dashboard;
